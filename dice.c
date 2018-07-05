cd81642f4a50bf30ab798310a463d7fc77d72395
s1250098 <s1250098@std3dc22>
2018-07-05 17:19:52 +0900

    Initial revision

---
+++ dice.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+
+  srand((unsigned int)time(NULL));
+  int x,y,z;
+  printf("Rolling the dice...\n");
+  x=rand()%6+1;
+  y=rand()%6+1;
+  z=x+y;
+  printf("Die 1:%2d\n",x);
+  printf("Die 2:%2d\n",y);
+  printf("Total value:%2d\n",z);
+  return 0;
+}
